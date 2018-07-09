1404d33ebb251360fc57df1019c86c80cd5f0768
Richard <richard.castagna@gmail.com>
2018-07-08 21:44:28 -0500

    adding project files

---
+++ src/components/maps.vue
@@ -0,0 +1,102 @@
+
+<template>
+    <div class="container">
+        <h1 class="header-padding">Google Maps Coding Test</h1>
+      <div class="row">
+          <div class="col col-5">
+              <label>
+                  Add Marker
+                  <span class="form-inline">
+                      <GmapAutocomplete @place_changed="setPlace" class="form-control form-control-md">
+                      </GmapAutocomplete>
+                      <button @click="usePlace" class="btn btn-primary btn-padding">Add</button>
+                  </span>
+              </label>
+              <br style="clear:both;" />
+              <label class="input-padding">
+                  Remove Marker
+                  <span class="form-inline">
+                      <GmapAutocomplete @place_changed="setPlace" class="form-control form-control-md">
+                      </GmapAutocomplete>
+                      <button @click="removePlace" class="btn btn-secondary btn-padding">Remove</button>
+                  </span>
+              </label>
+          </div>
+          <div class="col col-7">
+
+            <GmapMap style="width: 100%; height: 500px;" :zoom=this.zoom :center=this.center >
+              <GmapMarker v-for="(marker, index) in markers"
+                          :key="index"
+                          :position="marker.position"
+              />
+              <GmapMarker
+                      v-if="this.place"
+                      label="★"
+                      :position="{
+                  lat: this.place.geometry.location.lat(),
+                  lng: this.place.geometry.location.lng(),
+                }"
+              />
+            </GmapMap>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                markers: [],
+                place: null,
+                center: {lat:41.2565,lng:-95.9345},
+                zoom: 3
+            }
+        },
+        methods: {
+            setPlace(place) {
+                this.place = place
+            },
+            usePlace() {
+                if (this.place) {
+                    this.markers.push({
+                        position: {
+                            lat: this.place.geometry.location.lat(),
+                            lng: this.place.geometry.location.lng(),
+                        }
+                    })
+                    this.center = {
+                        lat: this.place.geometry.location.lat(),
+                        lng: this.place.geometry.location.lng(),
+                    }
+                    this.zoom = 10
+
+                    this.place = null;
+                }
+            },
+            removePlace(){
+                if(this.place){
+                    for(let iterator = 0; iterator <= this.markers.length; iterator ++){
+                        if (this.markers[iterator].position.lat == this.place.geometry.location.lat() && this.markers[iterator].position.lng == this.place.geometry.location.lng()){
+                            this.markers.splice(iterator, 1)
+                            this.place = null;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+</script>
+<style>
+    .header-padding{
+        padding-bottom:40px;
+    }
+    .input-padding{
+        padding-top:25px;
+    }
+    .btn-padding{
+        margin-left:10px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyB7a04Vkgao5q5ZM8TJrkNLt6OGuDM_5n0',
+        libraries: 'places', // This is required if you use the Autocomplete plugin
+    }
+})
+
+
+
+
